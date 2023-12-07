35b4ff2412db88939223d410bb47d2e6a1c951a1
Sergio Junior Castro Egurrolar <sergiojuniorce@gmail.com>
2023-12-06 19:11:57 -0700

    documentacion de la clase vehiculo

--- Implementacion/src/objetosNegocio/Vehiculo.java
+++ Implementacion/src/objetosNegocio/Vehiculo.java
@@ -5,34 +5,58 @@
 package objetosNegocio;
 
 /**
- *
+ * Clase que representa un vehiculo
  * @author fabricio01352
  */
 public class Vehiculo {
     private int km;
     private String nomVehiculo;
 
+    /**
+     * Constructor de la clase Vehiculo
+     * @param km Kilometraje del vehiculo
+     * @param nomVehiculo Nombre del vehiculo
+     */
     public Vehiculo(int km, String nomVehiculo) {
         this.km = km;
         this.nomVehiculo = nomVehiculo;
     }
 
+    /**
+     * Constructor vacio de la clase Vehiculo
+     */
     public int getKm() {
         return km;
     }
 
+    /**
+     * Obtiene el kilometraje del vehiculo
+     * @param km Kilometraje del vehiculo
+     */
     public void setKm(int km) {
         this.km = km;
     }
 
+    /**
+     * Obtiene el nombre del vehiculo
+     * @return nomVehiculo
+     */
     public String getNomVehiculo() {
         return nomVehiculo;
     }
 
+    /**
+     * Establece el nombre del vehiculo
+     * @param nomVehiculo Nombre del vehiculo
+     */
     public void setNomVehiculo(String nomVehiculo) {
         this.nomVehiculo = nomVehiculo;
     }
 
+    /**
+     * Muestra los datos del vehiculo
+     * @return Datos del vehiculo en formato String
+     */
     @Override
     public String toString() {
         return "Vehiculo{" + "km=" + km + ", nomVehiculo=" + nomVehiculo + '}';
